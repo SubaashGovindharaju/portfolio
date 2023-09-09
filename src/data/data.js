e49877bbef3a937e9948b4eba2d432e63f30444a
Subaash G <104092882+SubaashGovindharaju@users.noreply.github.com>
2023-09-09 16:45:02 +0530

    first commit

---
+++ src/data/data.js
@@ -0,0 +1,28 @@
+
+
+export const data = [
+  {
+    id: 1,
+    img: 'https://screenshot-proxy.netlify.app/f_avif,fit_cover,s_336x210/https://d33wubrfki0l68.cloudfront.net/64d8990bd6ea5000081dd7a5/screenshot_2023-08-13-08-49-33-0000.png',
+    title: "Brewery",
+    desc: "",
+    link: "https://webcode-1-subaash.netlify.app/",
+    git: "https://github.com/SubaashGovindharaju/netlify-webcode-1",
+  },
+  {
+    id: 2,
+    img: 'https://screenshot-proxy.netlify.app/f_avif,fit_cover,s_336x210/https://d33wubrfki0l68.cloudfront.net/64d899925fbae900082c1321/screenshot_2023-08-13-08-52-10-0000.png',
+    title: "Tarvel Agency",
+    link: "https://day-22-sbadmin-subaash.netlify.app/",
+    git: "https://github.com/SubaashGovindharaju/day-22-react-app",
+  },
+  {
+    id: 3,
+    img: 'https://screenshot-proxy.netlify.app/f_avif,fit_cover,s_336x210/https://d33wubrfki0l68.cloudfront.net/64d899b992c3f8000886a6df/screenshot_2023-08-13-08-52-35-0000.png',
+    title: "Create user",
+    desc: "",
+    link: "https://day-23-task-subaash.netlify.app/",
+    git: "https://github.com/SubaashGovindharaju/day-23-react-app",
+  }
+  
+];
